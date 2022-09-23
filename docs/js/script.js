3ffec0d01cff720a5754b3749749ac952a6f110c
Lev Borodin <levborodin@MacBook-Air-Lev.local>
2022-09-23 20:35:49 +0300

    [newBranch] code restyling

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,23 +1,10 @@
-const res = document.querySelector('.result');
+let res = document.querySelector('span');
 const toCount = document.querySelector('.container__submit');
 toCount.addEventListener('click', countFunc);
 
 function countFunc(){
     const bcValue = document.getElementById('bcPrice').value;
     const dollarValue = document.getElementById('dollarPrice').value;
-    const valueResult = bcValue !== '' && dollarValue !== '' && bcValue >= 0 && dollarValue >= 0 ? dollarValue / bcValue : 0;
-    const resultElem = document.createElement('div');
-    resultElem.classList.toggle('all-result');
-    resultElem.innerHTML = `You can buy ${valueResult} BTC`
-    res.appendChild(resultElem);
-    toCount.removeEventListener('click', countFunc);
-    toCount.addEventListener('click', removeFunc);
-    toCount.innerHTML = 'RETRY'
-}
-function removeFunc(){
-    const resultElem = document.querySelector('.all-result');
-    resultElem.remove();
-    toCount.innerHTML = 'RESULT';
-    toCount.removeEventListener('click', removeFunc);
-    toCount.addEventListener('click', countFunc);
+    let valueResult = bcValue !== '' && dollarValue !== '' && bcValue > 0 && dollarValue > 0 ? dollarValue / bcValue : 0;
+    res.innerHTML = valueResult;
 }
